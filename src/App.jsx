3c7ed99d8ed3bf642d876b603b2282fa875c81c5
Exudias <editoo31@gmail.com>
2024-07-06 17:54:18 +0300

    Add basic API fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
+import fetchData from './fetchData';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [version, setVersion] = useState("");
+  const [champions, setChampions] = useState({});
+
+  useEffect(() => {
+    const versionURL = import.meta.env.DEV ? import.meta.env.VITE_DDRAGON_VERSION_URL_DEV : process.env.VITE_DDRAGON_VERSION_URL;
+    const championsURL = import.meta.env.DEV ? import.meta.env.VITE_DDRAGON_CHAMPIONS_URL_DEV : process.env.VITE_DDRAGON_CHAMPIONS_URL;
+
+    Promise.resolve(fetchData(versionURL))
+      .then(fetchedVersion => {
+        setVersion(fetchedVersion[0]); 
+      })
+      .catch(error => {
+        console.error('Error in promise chain: ', error);
+      });
+
+    Promise.resolve(fetchData(championsURL))
+      .then(fetchedChamions => {
+        setChampions(fetchedChamions); 
+      })
+      .catch(error => {
+        console.error('Error in promise chain: ', error);
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {Object.keys(champions).length > 0 && <h1>Champions #: {Object.keys(champions.data).length}</h1>}
+      {version && <h1>Version: {version}</h1>}
     </>
   )
 }
---
+++ src/fetchData.js
@@ -0,0 +1,28 @@
+async function fetchData(url) 
+{
+    try 
+    {
+        const response = await fetch(`/api${url}`, {
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.9,bg;q=0.8",
+                "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Origin": "https://developer.riotgames.com",
+                "X-Riot-Token": import.meta.env.DEV ? import.meta.env.VITE_RIOT_API_KEY : process.env.VITE_RIOT_API_KEY
+            }
+        });
+        if (!response.ok)
+        {
+            throw new Error('Network response was not OK');
+        }
+        const data = await response.json();
+        return data;
+    }
+    catch (error) 
+    {
+        console.error('Error fetching data: ', error);
+        throw error;
+    }
+}
+
+export default fetchData;
