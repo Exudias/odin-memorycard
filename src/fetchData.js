679f5924d6ae7f7fd047066f38d0c2d92ad5b862
Exudias <editoo31@gmail.com>
2024-07-06 18:26:08 +0300

    Further testing

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,8 +1,10 @@
 async function fetchData(url) 
 {
+    const urlToUse = import.meta.env.DEV ? `/api${url}` : url;
+
     try 
     {
-        const response = await fetch(`/api${url}`, {
+        const response = await fetch(urlToUse, {
             headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
                 "Accept-Language": "en-US,en;q=0.9,bg;q=0.8",
