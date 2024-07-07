954bea1ea2d104d3b14089faea645b068cb2389f
Exudias <editoo31@gmail.com>
2024-07-07 11:00:14 +0300

    Cache API data in order to not waste calls

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,35 @@ import { useEffect, useState } from 'react';
 import './App.css'
 import fetchData from './fetchData';
 
+const STORAGE_VERSION_KEY = "version";
+const STORAGE_CHAMPIONS_KEY = "champions";
+
 function App() {
-  const [version, setVersion] = useState("");
-  const [champions, setChampions] = useState({});
+  const [version, setVersion] = useState(sessionStorage.getItem(STORAGE_VERSION_KEY) ?? "");
+  const [champions, setChampions] = useState(JSON.parse(sessionStorage.getItem(STORAGE_CHAMPIONS_KEY)) ?? {});
+
+  function updateVersion(newVersion)
+  {
+    setVersion(newVersion);
+    sessionStorage.setItem(STORAGE_VERSION_KEY, newVersion);
+  }
+
+  function updateChampions(newChampions)
+  {
+    setChampions(newChampions);
+    sessionStorage.setItem(STORAGE_CHAMPIONS_KEY, JSON.stringify(newChampions));
+  }
 
   useEffect(() => {
+    if (sessionStorage.getItem(STORAGE_CHAMPIONS_KEY)) return;
     const versionURL = '/api/versions.json';
 
     Promise.resolve(fetchData(versionURL))
       .then(fetchedVersion => {
-        setVersion(fetchedVersion[0]); 
+        updateVersion(fetchedVersion[0]); 
         Promise.resolve(fetchData(`/cdn/${fetchedVersion[0]}/data/en_US/champion.json`))
         .then(fetchedChamions => {
-          setChampions(fetchedChamions); 
+          updateChampions(fetchedChamions); 
         })
         .catch(error => {
           console.error('Error in promise chain: ', error);
